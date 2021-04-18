7e8731187d5c081bc0c53bf2143fe1491893c6a0
mikekibz59 <mikekibz59@gmail.com>
2021-04-18 16:35:36 +0300

    added sns publishe and abstractions

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -18,3 +18,4 @@ export const awsAccessKey = String(process.env.AWS_ACCESS_KEY);
 export const s3BucketName = String(process.env.S3_BUCKET_NAME);
 export const region = String(process.env.AWS_REGION);
 export const signingKey = String(process.env.SIGNING_KEY)
+export const topicArn = String(process.env.TOPIC_ARN)
---
+++ src/services/publisher/interfaces.ts
@@ -0,0 +1,28 @@
+/**
+ * Publisher interfaces file
+ *
+ * @format
+ */
+
+export interface HookResponse {
+	Provider: string;
+	timestamp: number;
+	type: string;
+}
+
+export interface SnsObject {
+	Message: string;
+	TopicArn: string;
+}
+
+export interface SNSPublisher {
+	publish: (data: SnsObject) => Promise<Object | undefined>;
+}
+
+interface requestId{
+	RequestId: string;
+}
+export interface PublisherResponse{
+	ResponseMetadata: requestId;
+	MessageId: string;
+}
